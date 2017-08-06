bd6dec243d019fda8eb0cf3c04da7649098dcb71
C80609A <c080609a@gmail.com>
2017-08-06 10:01:56 +0300

    UsersController complete.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,21 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
 
   # GET users
   def index
+    @users = User.all
+    json_response(@users)
   end
 
   # GET users/1
   def show
+    json_response(@user)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
