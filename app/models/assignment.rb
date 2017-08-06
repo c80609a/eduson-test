a60c3860c07775d717c9df1935128176bf83b70d
C80609A <c080609a@gmail.com>
2017-08-06 08:01:53 +0300

    assignment: minor fix: 'You tried to define an enum named "status" on the model "Assignment", but this will generate a class method "new", which is already defined by Active Record'.

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,13 +1,13 @@
 class Assignment < ActiveRecord::Base
   belongs_to :user
   belongs_to :task
-  enum status: [:new, :in_progress, :done, :approved, :declined]
+  enum status: [:fresh, :in_progress, :done, :approved, :declined]
   after_initialize :set_default_status, :if => :new_record?
 
   private
 
   def set_default_status
-    self.status ||= :new
+    self.status ||= :fresh
   end
 
 end
